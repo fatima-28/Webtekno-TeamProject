6a1d94f7b53640bd57bb847b864c91614917f6c2
fatima-28 <2n6gddb@code.edu.az>
2022-12-11 10:53:21 -0800

    problem fixing

---
+++ src/layouts/Products/All-Products/AllProducts.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import './AllProducts.scss';
+import ProductCards from "../../../components/AllProductsCards/AllProductsCards";
+import Images from "../../../components/images"
+
+const AllProducts = () => {
+    let products = document.querySelector(".main-products")
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        fetch("  http://localhost:3000/cards").then(
+
+            response => response.json().then(data => {
+                setImages(data);
+            })
+        )
+        //fixing
+
+    }, [])
+    return (
+        <section className='all-products'>
+            <div className="main-products">
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+                <ProductCards />
+            </div>
+            <div>
+                <Images data={images} />
+            </div>
+        </section>
+    )
+}
+export default AllProducts
